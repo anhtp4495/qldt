2f3e933942340bb5f6c32d7c2e937d464dca306e
emotive_atr <anh.tran@emotive-software.com>
2023-06-02 14:00:15 +0700

    change sql server (express - free license) ip

--- CNTT129_NetCore/Settings/AppSettings.cs
+++ CNTT129_NetCore/Settings/AppSettings.cs
@@ -2,7 +2,7 @@
 {
     public class AppSettings
     {
-        public const string ConnectionString = "Data Source=35.229.245.0,1433;Initial Catalog=hufi;User ID=hufi;Password=g)C*pUO#z&f-SO{9;";
+        public const string ConnectionString = "Data Source=34.80.74.207,1433;Initial Catalog=hufi;User ID=hufi;Password=g)C*pUO#z&f-SO{9;";
         public const string ApiPrefix = "api";
         public const string ApiVersion = "v1";
     }
